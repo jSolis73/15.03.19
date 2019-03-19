6a8ba545c64c18f9fa9526b41ec8fb0ef6f27828
unknown <chez90@mail.ru>
2019-03-19 18:32:26 +0400

    последнее изменение

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ChangeStr from './task12/12'
+import ChangeSt from './task12/12-1'
 
+export let st = {mes1: true}
+Vue.component('firstComp', ChangeStr)
+Vue.component('secondComp', ChangeSt)
 new Vue({
   el: '#app',
   render: h => h(App)
---
+++ src/task12/12-1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p v-if = "mes2">Здесь мог быть ваш код</p>
+        <p v-else>Теперь здесь мой код</p>
+        <button @click="mes2 = !mes2">Изменить текст</button>
+    </div>
+</template>
+<script>
+let cc ={mes2: true}
+export default {
+    data(){
+        return cc 
+    }
+        
+    
+}
+</script>
