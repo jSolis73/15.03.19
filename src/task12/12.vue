984a0ebc3be9bd3c808c5a6516134f1c72c19e33
unknown <chez90@mail.ru>
2019-03-19 14:03:54 +0400

    Создал компонент и подключил его. Приложение состоит из надписи и кнопки. Все экземпляры этого компонента работают синхронно.

---
+++ src/task12/12.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p v-if = "mes">Здесь мог быть ваш код</p>
+        <p v-else>Теперь здесь мой код</p>
+        <button @click="mes = !mes">Изменить текст</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+        mes: true
+        }
+    } 
+}
+</script>
