6be0ac1f65e2d46008023600d47f0103a5527125
unknown <chez90@mail.ru>
2019-03-20 18:12:32 +0400

    Создал 2 компонента. Родительский(user) принимает дочерний компонет inform. Оба компонента зарегестрированы локально во входном файле App.vue. Компонент user передает 3 параметра в дочерний компонент. Уже там они выводятся в шаблон, также проводится валидация этих параметров при помощи методов и вычисляемых свойств.

---
+++ src/components/inform.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h3>Пользователь</h3>
+        <p id = "ages">Возраст: {{age}}.  <button @click = "ageTest">Проверить на ограничение по возрасту</button></p>
+        <p>Страна: {{country}}. Прописные буквы: {{largeSymb}}</p>
+        <p>Пол: {{gender}}. Сокращение: {{cut}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    //props: ['age', 'country', 'gender'],
+    props: {
+        age: {
+            type: Number,
+            required: true
+        },
+        country: {
+
+        },
+        gender: {
+
+        }
+    },
+    data(){
+        return {
+        
+        }
+    },
+    computed: {
+        largeSymb(){
+            return this.country.toUpperCase()
+        },
+        cut(){
+            return this.gender.substring(0,3)
+        }
+    },
+    methods: {
+        ageTest() { 
+            let dd = this.age;
+            if (dd < 18) {
+                let years = document.getElementById('ages');
+                years.outerText ='Извините, вы нам не подходите'; 
+            }                    
+        }
+    }
+}
+</script>
---
+++ src/components/user.vue
@@ -0,0 +1,25 @@
+<template>
+    <inform-app :age="age"
+                :country="country"
+                :gender="gender"
+    >
+    </inform-app>
+</template>
+
+<script>
+import Inform from './inform.vue'
+export default {
+    data(){
+        return{
+            age: 24,
+            country: 'Германия',
+            gender: 'мужской'
+        }
+    },
+    components: {
+        informApp: Inform
+    }
+}
+</script>
+
+
