b466d1006e40d72d37bc15439c6d803c6693c014
unknown <chez90@mail.ru>
2019-04-20 19:52:43 +0400

    create div, which display all information from the form

--- src/components/formFields.vue
+++ src/components/formFields.vue
@@ -1,36 +1,45 @@
 <template>
     <div>
-        <form>
+        <form v-show = "show">
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" v-model = "email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">About me</label>
-                <input type="email" v-model = "about" class="form-control" id="about" aria-describedby="emailHelp" placeholder="Your name, age, etc">
+                <input type="email" v-model = "about" class="form-control" id="about" placeholder="Your name, age, etc">
             </div>
             <div class="form-group form-check">
-                <input type="checkbox" v-model = "checkedFields" class="form-check-input" id="sendNews">
+                <input type="checkbox" v-model = "checkedFields" class="form-check-input" id="sendNews" value = "Send news">
                 <label class="form-check-label" for="exampleCheck1">Send news</label>
             </div>
             <div class="form-group form-check">
-                <input type="checkbox" v-model = "checkedFields" class="form-check-input" id="acceptRules">
+                <input type="checkbox" v-model = "checkedFields" class="form-check-input" id="acceptRules" value = "accept rules">
                 <label class="form-check-label" for="exampleCheck1">accept rules</label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" v-model = "gender" type="radio" name="exampleRadios" id="male" value="option1">
+                <input class="form-check-input" v-model = "gender" type="radio" name="radio1" id="male" value="male">
                 <label class="form-check-label" for="exampleRadios1">
                     male
                 </label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" v-model = "gender" type="radio" name="exampleRadios" id="female" value="option2">
+                <input class="form-check-input" v-model = "gender" type="radio" name="radio2" id="female" value="female">
                 <label class="form-check-label" for="exampleRadios2">
                     female
                 </label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" @click = "show = !show">Submit</button>
         </form>
+        <div v-show = "!show">
+            <p>Email adress: {{email}}</p>
+            <p>About me: {{about}}</p>
+            <p>Options</p>
+            <ul>
+                <li v-for = "option in checkedFields" :key = "option"> {{option}} </li>
+            </ul>
+            <p>Gender: {{gender}}</p>
+        </div> 
     </div>    
 </template>
 
@@ -38,6 +47,7 @@
 export default {
     data() {
         return {
+            show: true,
             email: '',
             about: '',
             checkedFields: [],
