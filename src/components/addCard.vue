943655fd0e2849fb8262280a6e3f5c06f8330cf2
unknown <chez90@mail.ru>
2019-04-23 22:11:01 +0400

    delete method

--- src/components/addCard.vue
+++ src/components/addCard.vue
@@ -3,16 +3,16 @@
         <div class="form-group row">
             <label class="col-sm-2 col-form-label mt-2" style="padding-left: 230px">Title</label>      
             <div class="col-sm-10">
-                <textarea v-model = "title" id = "title" class="form-control mt-3" style="width: 40%"></textarea>    
+                <textarea v-model="title" class="form-control mt-3" style="width: 40%"></textarea>    
             </div>
             
             <label class="col-sm-2 col-form-label mt-2" style="padding-left: 230px">Text</label>
             <div class="col-sm-10">
-                <textarea v-model = "text" id = "text" class="form-control mt-3" style="width: 40%"></textarea>
+                <textarea v-model="text" class="form-control mt-3" style="width: 40%"></textarea>
             </div> 
             
             <div class="col-sm-10" style="padding-left: 230px">    
-                <button type = "button" @click = "createCard" class="btn btn-primary mt-3">Add Card</button>
+                <button type="button" @click = "this.arrayCards.push({title, text})" class="btn btn-primary mt-3">Add Card</button>
             </div>             
         </div>    
     </div>
@@ -25,19 +25,6 @@ export default {
         return {
             title: '',
             text: ''
-        }
-    },
-    methods: {
-        createCard() {
-            let title = this.title;
-            let text = this.text;
-            if (document.getElementById('title').value != '' && document.getElementById('text').value != '') {  
-                this.arrayCards.push({title, text});
-                setTimeout (function clearProps() {
-                    document.getElementById('title').value = '';
-                    document.getElementById('text').value = '';  
-                }, 100);
-            }  
         }
     }
 }
