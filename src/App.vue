ce86bf92acc69a69bf4c1e1a898cbae446980bac
unknown <chez90@mail.ru>
2019-05-07 20:10:38 +0400

    create filter, not working yet

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    
+   <h4>{{firstDate | dateDiffertence}}</h4> 
   </div>
 </template>
 
 <script>
 
 export default {
+  filters: {
+    dateDifference (value) {
+      value = Date.now() / 1000 - firstDate.getTime() / 1000
+      if (value <= 60)
+        return "Только что";
+      else if (value <= 3600)
+        return value / 60 + "минут назад";
+      else if (value >= 3600 && value < 3600 * 24)
+        return firstDate.getHours() + firstDate.getMinutes();
+      else if (value >= 3600 * 24)
+        return firstDate.getDate() + firstDate.getMonth();
+    }
+  },
   data() {
     return {
-    
+      firstDate: new Date (2019, 4, 7),
+
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import moment from 'moment'
 
-Vue.use (moment)
+
 new Vue({
   el: '#app',
   render: h => h(App)
