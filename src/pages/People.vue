b468fbf0d13ffd433d246a17e7a55369e84b7a4f
unknown <chez90@mail.ru>
2019-05-23 21:02:20 +0400

    create page "team"

---
+++ src/pages/People.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <table class="table table-bordered">  
+            <tbody>
+                <tr>
+                    <td>
+                        <p>Василий</p>
+                    </td>
+                    <td>
+                        <p>Семён</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Анатолий
+                    </td>
+                    <td>
+                        Станислав
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import VueRouter from 'vue-router'
 import Home from './pages/Home'
+import People from './pages/People'
+
 export default new VueRouter({
     routes: [
         {
             path: '',
             component: Home
+        },
+        {
+            path: '/team',
+            component: People
         }
     ],
     mode: 'history'
