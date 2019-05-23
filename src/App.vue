6775b75a47d8b6989c6a7557440a14d5a485927a
unknown <chez90@mail.ru>
2019-05-23 21:13:43 +0400

    create page "About", add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Главная</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Наша команда</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О нас</a>
+      </li>
+    </ul>
+
     <router-view></router-view>  
   </div>
 </template>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import Home from './pages/Home'
 import People from './pages/People'
+import About from './pages/About'
 
 export default new VueRouter({
     routes: [
@@ -11,6 +12,10 @@ export default new VueRouter({
         {
             path: '/team',
             component: People
+        },
+        {
+            path: '/about',
+            component: About
         }
     ],
     mode: 'history'
